28b3bfc8e5e7322a42efd9af4c2e901cf7a8e02c
Fabien Grignoux <fabien.grignoux@outlook.com>
2023-01-12 13:45:20 +0100

    Ajout de nodemon et accueil utilisateur
    Installation de Nodemon
    "npm i -D nodemon"
    
    puis dans le package.json
    "dev": "tsc -w & nodemon dist",

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,11 @@
-const firstname = "Fabien";
-console.log(`Hello ${firstname}`);
+const message = "Rapports de l'entreprise";
+const companyName = "companyName";
+
+const dateObject = new Date();
+const todayDate = dateObject.toLocaleDateString();
+
+console.log(`${message} ${companyName} !`);
+
+console.log(
+  `--------------------------Rapport du ${todayDate}--------------------------`
+);
