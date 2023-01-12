e0a5682193418198b038f864064dee7ed256e462
Fabien Grignoux <fabien.grignoux@outlook.com>
2023-01-12 15:33:27 +0100

    Unit test
    npm i -D vitest
    
    dans le package.json ajouter
    "test":"vitest" dans les scrips

---
+++ src/__tests__/analyzer.spec.ts
@@ -0,0 +1,47 @@
+import Analyzer from "../analyzer";
+import IAnalyzer from "../interfaces/iAnalyzer";
+import IEmployee from "../interfaces/iEmployee";
+import { describe, expect, beforeEach, it } from "vitest";
+
+const datas: IEmployee[] = [
+  {
+    id: "ALf96FV",
+    firstname: "Léa",
+    lastname: "Dumond",
+    salary: 58000,
+    job: "CEO",
+    gender: "f",
+  },
+  {
+    id: "PO5fdII",
+    firstname: "Romain",
+    lastname: "Santos",
+    salary: 48000,
+    job: "commercial",
+    gender: "m",
+  },
+];
+
+describe(">>>>>>>>>>analyzer class", () => {
+  let analyzer: IAnalyzer;
+
+  beforeEach(() => {
+    analyzer = new Analyzer(datas);
+  });
+
+  it("should return total salaries", () => {
+    const expected = 106000;
+
+    const result = analyzer.getTotalSalaries();
+
+    expect(result).toBe(expected);
+  });
+
+  it("should return number of gender female and gender male", () => {
+    const expected = { nbGenderM: 1, nbGenderF: 1 };
+
+    const result = analyzer.getGenderRepartition();
+
+    expect(result).toStrictEqual(expected); //pour les objets
+  });
+});
